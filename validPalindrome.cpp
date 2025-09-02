36424b45c13d8d84b4ebab2bfe5342f99401c6ad
natan kassa <nkassa@alumni.usc.edu>
2025-09-02 00:31:33 -0700

    validPalin

---
+++ validPalindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    {
+        string str = "";
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(isalnum(s[i]))
+            {
+                str += tolower(s[i]);
+            }
+        }
+        int left = 0;
+        int right = str.size()-1;
+        while(left <= right)
+        {
+            if(str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
